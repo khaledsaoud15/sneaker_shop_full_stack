1161428d6c076ba5b2b82dd1700a399722a320b0
unknown <khaledsaoud237@gmail.com>
2025-01-10 15:31:47 +0100

    habmurger menue logic responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,66 @@
 import { Search } from "@mui/icons-material";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const MobileMenue = ({ active }) => {
   return (
-    <nav className="flex flex-row-reverse justify-between h-20 bg-white px-8 items-center w-full lg:flex-row lg:px-16">
+    <div
+      className={`absolute top-full left-0 flex flex-col w-full ${
+        active ? "h-fit" : "h-0"
+      } px-16 gap-6 bg-white z-10 overflow-hidden transition-all duration-300`}
+    >
+      <Link to="/" className="w-fit mt-8">
+        Home
+      </Link>
+      <Link to="/about" className="w-fit">
+        About
+      </Link>
+      <a href="" className="flex items-center w-fit">
+        Shop <ArrowDropDownIcon />
+      </a>
+      <a href="#" className="flex items-center w-fit">
+        Blog <ArrowDropDownIcon />
+      </a>
+      <a href="" className="w-fit">
+        Account
+      </a>
+      <a href="" className="w-fit">
+        Wishlist
+      </a>
+      <a className="flex items-center gap-1 w-fit" href="">
+        Account
+        <a
+          href=""
+          className="w-4 h-4 p-3 rounded-full bg-gray-100 flex border border-black items-center justify-center"
+        >
+          0
+        </a>
+      </a>
+      <Search className="!text-md cursor-pointer mb-8" />
+    </div>
+  );
+};
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const resize = () => {
+      window.addEventListener("resize", () => {
+        if (window.innerWidth > 1020) {
+          setIsActive(false);
+        }
+      });
+    };
+
+    resize();
+
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  return (
+    <nav className="relative flex flex-row-reverse justify-between h-20 bg-white px-8 items-center w-full lg:flex-row lg:px-16">
       <div className="hidden items-center gap-8  lg:flex ">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
@@ -16,11 +71,15 @@ const Header = () => {
           Blog <ArrowDropDownIcon />
         </a>
       </div>
-      <div className="flex flex-col gap-1 cursor-pointer lg:hidden">
+      <div
+        className="flex flex-col gap-1 cursor-pointer lg:hidden"
+        onClick={() => setIsActive(!isActive)}
+      >
         <span className="w-8 h-1 rounded-full bg-black"></span>
         <span className="w-6 h-1 ml-auto rounded-full bg-black"></span>
         <span className="w-4 h-1 ml-auto rounded-full bg-black"></span>
       </div>
+      <MobileMenue active={isActive} />
       <img src="/logo.svg" alt="" className="h-fit " />
 
       <div className="flex items-center gap-4">
